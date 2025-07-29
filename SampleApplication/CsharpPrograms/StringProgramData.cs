a98cbcab7b686ca851fd64a7bd0b9d0cde07d246
devesh <admin@DESKTOP-V7DKBUC>
2025-07-29 09:27:13 -0700

    string program

---
+++ SampleApplication/CsharpPrograms/StringProgramData.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace SampleApplication.CsharpPrograms
+{
+    internal class StringProgramData
+    {
+        string stringData = "Hello this is devesh";
+        // devesh is this hello
+
+
+
+        public string printreverse( string str)
+        {
+            //str = "Ramkumar";
+            // ramukmar 
+            StringBuilder sb = new StringBuilder();
+            char[] strdata = str.ToCharArray();
+            for (int i = strdata.Length; i-- >= 0;)
+            {
+                sb.Append(strdata[i]);
+                sb.Append(" ");
+                if (i == 0)
+                    break;
+            }
+            return sb.ToString();
+
+        }
+        public string ReverseTheString(string inputString)
+        {
+            string[] array = inputString.Split(" ");
+            StringBuilder sb = new StringBuilder(); 
+
+
+            for (int i = array.Length ; i-- >=0;) {
+                sb.Append(array[i]);
+                sb.Append(" ");
+                if (i == 0)
+                    break;
+            }
+
+
+            return sb.ToString();
+
+        }
+        // Name->Devesh,  Address , City->India, State, Country
+        public string ReverseTheStringWithSpecialSybmbol(string inputString, char csplit)
+        {
+            string[] array = inputString.Split(csplit);
+            StringBuilder sb = new StringBuilder();
+
+
+            for (int i = array.Length; i-- >= 0;)
+            {
+                sb.Append(array[i]);
+                sb.Append(" ");
+                if (i == 0)
+                    break;
+            }
+
+
+            return sb.ToString();
+
+        }
+    }
+}
--- SampleApplication/Program.cs
+++ SampleApplication/Program.cs
@@ -1,7 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using SampleApplication.CsharpPrograms;
 using SampleApplication.InterfaceWithDI;
 using System.Runtime.CompilerServices;
 using System.Text;
 using static SampleApplication.Delegates.MathDelegatesExample;
 
+StringProgramData obj = new StringProgramData();
 
+string data= obj.ReverseTheString("DEvesh omar");
+
+obj.ReverseTheStringWithSpecialSybmbol("This-is-my-room",'-');
+
+var datavalue=obj.printreverse("Rajkumar");
+
+Console.WriteLine(  data);
